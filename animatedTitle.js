075f8af94fb7425b62ee1e3bfba07fb043ba7893
Adriel Ripoll <88801947+Adrielripoll@users.noreply.github.com>
2021-12-09 17:50:48 -0300

    Add files via upload

---
+++ animatedTitle.js
@@ -0,0 +1,50 @@
+var _CONTENT = [ 
+	'O CAPS LOCK ESTAVA LIGADO?', 
+	'o caps lock estava desligado?', 
+	'esqueceu das iniciais maiúsculas?' 
+];
+
+var _PART = 0;
+var _PART_INDEX = 0;
+var _INTERVAL_VAL;
+var _ELEMENT = document.querySelector("#text");
+var _CURSOR = document.querySelector("#cursor");
+
+function Type() { 
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	if(text === _CONTENT[_PART]) {
+		_CURSOR.style.display = 'none';
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 5000);
+	}
+}
+
+
+function Delete() {
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+
+_INTERVAL_VAL = setInterval(Type, 100);
